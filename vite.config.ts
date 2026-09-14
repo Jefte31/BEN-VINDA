58233ab1c00a54440601176ec7dc7ba125937293
Jefte31 <targinojefte@gmail.com>
2026-09-14 10:44:52 -0300

    fix: usa caminhos relativos no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
-  base: process.env.GITHUB_ACTIONS ? '/BEN-VINDA/' : '/',
+  base: './',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
