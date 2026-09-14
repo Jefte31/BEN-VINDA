13bffcf3f00f586e0355ad7c881d7869ddb21c89
Jefte31 <targinojefte@gmail.com>
2026-09-14 10:45:07 -0300

    fix: evita tela branca e carrega estilos da galeria

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { SiteErrorBoundary } from './components/SiteErrorBoundary';
 import './index.css';
 import './brand.css';
+import './media.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SiteErrorBoundary>
+      <App />
+    </SiteErrorBoundary>
   </StrictMode>,
 );
